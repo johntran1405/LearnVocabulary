44235d0363e8f04d9cd117828a047a6067c8fb71
Nguyen Quang Viet <quangvietds3@gmail.com>
2017-03-23 15:23:32 +0700

    main

--- app/src/main/java/ccwav/blogspot/com/learnvocabulary/MainActivity.java
+++ app/src/main/java/ccwav/blogspot/com/learnvocabulary/MainActivity.java
@@ -1,9 +1,13 @@
 package ccwav.blogspot.com.learnvocabulary;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button btnHome,btnPeople,btnFruit,btnAnimal,btnSport,btnTravel,btnVegetable,btnFood;
+    private TextView txthome,txtpeople,txtfruit,txtanimal,txtsport,txttravel,txtvegetable,txtfood;
 //    RelativeLayout R1, R2, R3, R4, R5, R6, R7, R8;
     private Animation scale1,scale2,scale3,scale4,scale5,scale6,scale7,scale8;
     private final int TIME_DELAY_SCALE_BTN = 700;  // time delay for topic  button
@@ -61,32 +66,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void addControl() {
-//        int ImageId1=this.getResources().getIdentifier(listcate.get(0).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId2=this.getResources().getIdentifier(listcate.get(1).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId3=this.getResources().getIdentifier(listcate.get(2).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId4=this.getResources().getIdentifier(listcate.get(3).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId5=this.getResources().getIdentifier(listcate.get(4).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId6=this.getResources().getIdentifier(listcate.get(5).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId7=this.getResources().getIdentifier(listcate.get(6).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        int ImageId8=this.getResources().getIdentifier(listcate.get(7).getCategori_Icon().toLowerCase(),"drawable",getPackageName());
-//        Log.d("hinh", String.valueOf(ImageId2));
         btnHome = (Button) findViewById(R.id.btn_home);
-        //btnHome.setBackgroundResource(ImageId1);
+        btnHome.setBackground(getImage(listcate.get(0).getCategori_Icon()));
+        txthome= (TextView) findViewById(R.id.txthome);
+        txthome.setText(listcate.get(0).getCategori_Name());
+
+
         btnPeople = (Button) findViewById(R.id.btn_people);
-        //btnPeople.setBackgroundResource(ImageId2);
+        btnPeople.setBackground(getImage(listcate.get(4).getCategori_Icon()));
+        txtpeople= (TextView) findViewById(R.id.txtpeople);
+        txtpeople.setText(listcate.get(4).getCategori_Name());
+
+
         btnAnimal = (Button) findViewById(R.id.btn_animal);
-        //btnAnimal.setBackgroundResource(ImageId3);
+        btnAnimal.setBackground(getImage(listcate.get(1).getCategori_Icon()));
+        txtanimal= (TextView) findViewById(R.id.txtanimal);
+        txtanimal.setText(listcate.get(1).getCategori_Name());
+
+
         btnFruit = (Button) findViewById(R.id.btn_fruit);
-        //btnFruit.setBackgroundResource(ImageId4);
+        btnFruit.setBackground(getImage(listcate.get(7).getCategori_Icon()));
+        txtfruit= (TextView) findViewById(R.id.txtfruit);
+        txtfruit.setText(listcate.get(7).getCategori_Name());
+
+
         btnSport = (Button) findViewById(R.id.btn_sport);
-        //btnSport.setBackgroundResource(ImageId5);
+        btnSport.setBackground(getImage(listcate.get(6).getCategori_Icon()));
+        txtsport= (TextView) findViewById(R.id.txtsport);
+        txtsport.setText(listcate.get(6).getCategori_Name());
+
+
         btnTravel = (Button) findViewById(R.id.btn_travel);
-        //btnTravel.setBackgroundResource(ImageId6);
+        btnTravel.setBackground(getImage(listcate.get(2).getCategori_Icon()));
+        txttravel= (TextView) findViewById(R.id.txttravel);
+        txttravel.setText(listcate.get(3).getCategori_Name());
+
+
         btnVegetable = (Button) findViewById(R.id.btn_vegetable);
-        //btnVegetable.setBackgroundResource(ImageId7);
+        btnVegetable.setBackground(getImage(listcate.get(5).getCategori_Icon()));
+        txtvegetable= (TextView) findViewById(R.id.txtvegetable);
+        txtvegetable.setText(listcate.get(5).getCategori_Name());
+
+
         btnFood = (Button) findViewById(R.id.btn_food);
-        //btnFood.setBackgroundResource(ImageId8);
+        btnFood.setBackground(getImage(listcate.get(3).getCategori_Icon()));
+        txtfood= (TextView) findViewById(R.id.txtfood);
+        txtfood.setText(listcate.get(3).getCategori_Name());
 
 
       //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -114,6 +141,20 @@ public class MainActivity extends AppCompatActivity {
 
         tv_learnEnglish = (TextView) findViewById(R.id.tv_learnenglish);
 
+    }
+
+    public Drawable getImage(String nameimg)
+    {
+        int img = getResources().getIdentifier(nameimg, "drawable",getApplicationContext().getPackageName());
+        Log.d("Hinh","TenHinh"+ img);
+        Drawable dr;
+//        if(img!=0) {
+            dr = getApplicationContext().getResources().getDrawable(img);
+//        }else
+//        {
+//            dr = getApplicationContext().getResources().getDrawable(R.drawable.home);
+//        }
+        return dr;
     }
 
     private void addEvent() {
