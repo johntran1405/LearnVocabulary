377f4090ccfd090216252ba0887a527a8817912a
quangsangtt2 <voquangsangtt2@gmail.com>
2017-03-23 17:04:29 +0700

    getallwordbycate

--- app/src/main/java/ccwav/blogspot/com/learnvocabulary/Database/WordsSQLite.java
+++ app/src/main/java/ccwav/blogspot/com/learnvocabulary/Database/WordsSQLite.java
@@ -3,6 +3,7 @@ package ccwav.blogspot.com.learnvocabulary.Database;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -37,6 +38,28 @@ public class WordsSQLite extends MyDatabase {
 //                +" VN: "+listwords.get(1).getVietnamese()
 //                +" SPe: "+listwords.get(1).getSpeech()
 //                +" IM: "+listwords.get(1).getImage());
+        return listwords;
+    }
+
+    public ArrayList<Words_Model> getAllWordsbyCategori(int id) {
+        ArrayList<Words_Model> listwords = new ArrayList<Words_Model>();
+        try {
+            openDataBase();
+            Cursor cs = database.rawQuery("select * from Word where Categori_ID = id ORDER BY ramdom()", null);
+            Words_Model words_model;
+            while (cs.moveToNext()) {
+                words_model = new Words_Model(cs.getInt(0),cs.getInt(1),cs.getString(2), cs.getString(3),cs.getString(4), cs.getString(5), cs.getString(6));
+                listwords.add(words_model);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        Log.d("a","b" + getAllWordsbyCategori(3));
+
+
+              ;
         return listwords;
     }
 }
